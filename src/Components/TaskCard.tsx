180578aa5efffa046780a56f6dfbf7eba37322b8
zents <kyliandevhalal@gmail.com>
2026-04-11 18:04:10 +0200

    styled taskcard like buttons for future functionnality

--- src/Components/TaskCard.tsx
+++ src/Components/TaskCard.tsx
@@ -4,7 +4,7 @@ export function TaskCard({ id, title, endDate }) {
   return (
     <div
       id={id}
-      className="bg-card-dark shadow-xl flex flex-col py-6 px-4 rounded-xl w-full gap-y-2"
+      className="bg-card-dark shadow-xl flex flex-col py-6 px-4 rounded-xl w-full gap-y-2 cursor-pointer hover:bg-card-dark/60 transition-colors"
     >
       <span className="font-semibold text-lg">{title}</span>
       <span className="text-secondary-text text-sm font-semibold flex flex-row items-center gap-x-2">
