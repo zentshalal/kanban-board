1fb28080d46e2bde279325bfc95e75384e3a902c
zents <kyliandevhalal@gmail.com>
2026-04-11 07:10:59 +0200

    changed font family

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -3,9 +3,9 @@ import { EllipsisVertical } from 'lucide-react';
 export function Banner() {
   return (
     <header className="flex flex-row items-center justify-between bg-card-dark w-full px-6 py-6">
-      <div className="text-2xl font-medium">Default Board</div>
+      <div className="text-2xl font-semibold">Default Board</div>
       <div className="flex flex-row gap-x-2 items-center">
-        <button className="px-4 py-3 bg-action rounded-full font-medium hover:bg-action/80 transition-colors cursor-pointer">
+        <button className="px-4 py-3 bg-action rounded-full font-semibold hover:bg-action/80 transition-colors cursor-pointer">
           + Add New Task
         </button>
         <button className="p-2 rounded-full hover:bg-main-dark transition-colors cursor-pointer">
